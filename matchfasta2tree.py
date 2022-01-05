ccb847975d5c32191527f4039da65ffd5318a0be
rosepeterson <43347530+rosepeterson@users.noreply.github.com>
2022-01-04 20:35:49 -0500

    Add files via upload

--- matchfasta2tree.py
+++ matchfasta2tree.py
@@ -9,7 +9,7 @@ from Bio import SeqIO
 from Bio import Phylo
 
 
-parser = argparse.ArgumentParser(description="Written under python 3 and requires Biopython and dendropy. ")
+parser = argparse.ArgumentParser(description="Written under python 3 and requires Biopython")
 parser.add_argument('-f', '--fasta' , dest = 'fasta' , type = str , default= None , required= True, help = 'Fasta alignment to compare')
 parser.add_argument('-p', '--phylo', dest = 'phylo', type = str, default = None, required = True, help = 'phylogeny')
 parser.add_argument('-t', '--filetype', dest = 'filetype', type = str, default = None, required = True, help = 'phylogeny format: newick, nexus, nexml, phyloxml, or cdao')
@@ -22,12 +22,12 @@ parser.add_argument('-g', '--outalign', dest = 'outalign', type = str, default =
 args, unknown = parser.parse_known_args()
 
 
-def non_match_elements(list1, list2):
-    non_match = []
+def mismatch(list1, list2):
+    nomatch = []
     for i in list1:
         if i not in list2:
-            non_match.append(i)
-    return non_match
+            nomatch.append(i)
+    return nomatch
 
 
 list1 = []
@@ -39,11 +39,11 @@ trees = Phylo.read(args.phylo, args.filetype)
 list2 = [term.name for term in trees.get_terminals()]
 
 	
-non_match = non_match_elements(list1,list2)
+non_match = mismatch(list1,list2)
 print("Names not found in phylogeny", non_match)
 
 
-non_matchr = non_match_elements(list2,list1)
+non_matchr = mismatch(list2,list1)
 print("Names not found in alignment", non_matchr)
 
 if args.prunetree == 'True':
